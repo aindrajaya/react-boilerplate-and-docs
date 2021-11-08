6927f5f7d47feb942a2660816c3d7eed0fe832bc
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-11-08 13:08:51 +0700

    register comopnent

---
+++ components/UsersSaga/components/Register.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { registerStart } from '../store/authServices/authActions'
+
+export default function RegisterPage() {
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: ""
+  })
+
+  const dispatch = useDispatch()
+
+  const handleChange = (e) => setCredentials({
+    ...credentials,
+    [e.target.name] : e.target.value
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(registerStart(credentials))
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>REGSITER</h2>
+      <label>
+        Email
+        <input 
+          name="email"
+          type="text"
+          value={credentials.email}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Password
+        <input 
+          name="password"
+          type="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="submit">Register</button>
+    </form>
+  )
+}
